9d33f97cd62f06df528053662ef1695d91f202c3
Com6235 <ru.nms199@gmail.com>
2024-04-28 14:14:37 +0300

    Add a publish job

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import java.net.URI
+
 plugins {
     kotlin("jvm") version "1.9.23"
     id("io.freefair.lombok") version "8.6"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.jetbrains.dokka") version "1.9.20"
     idea
+    `maven-publish`
 }
 
 group = "io.github.com6235"
@@ -37,5 +39,18 @@ idea {
     module {
         isDownloadJavadoc = true
         isDownloadSources = true
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI("https://maven.pkg.github.com/com6235/tgbotter")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
