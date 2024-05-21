8dc19f65f1c42fe050e1cfe4d72b2caf878e31da
Com6235 <ru.nms199@gmail.com>
2024-05-21 16:23:28 +0300

    configurator: Add KDoc

--- configurator/src/main/kotlin/Loader.kt
+++ configurator/src/main/kotlin/Loader.kt
@@ -8,15 +8,28 @@ import kotlin.io.path.extension
 import kotlin.io.path.inputStream
 import kotlin.io.path.isDirectory
 
+/**
+ * Loader for configs, add custom loaders using [FileExtensions.addExtension].
+ * Use [loadConfig] to load configs.
+ * For this to work, you must have a data class with `@Serializable` annotation:
+ * ```
+ * @Serializable
+ * data class Human(val name: String)
+ *
+ * ConfigLoader(Human.serializer()).loadConfig(Path("./config.json"))
+ * ```
+ *
+ * @see Loader
+ */
 class ConfigLoader<T : Any>(serializer: KSerializer<T>) {
     val fileExtensions = FileExtensions(serializer)
 
-    fun loadConfig(stream: InputStream, fileExtension: String): Config<T> {
+    fun loadConfig(stream: InputStream, fileExtension: String): T {
         val loader = fileExtensions.findExtension(fileExtension) ?: throw NotSerializableException()
-        return Config(loader.load(stream))
+        return loader.load(stream)
     }
 
-    fun loadConfig(path: Path): Config<T>? {
+    fun loadConfig(path: Path): T? {
         if (!path.exists() || path.isDirectory()) return null
         return loadConfig(path.inputStream(), path.extension)
     }
--- configurator/src/main/kotlin/Others.kt
+++ configurator/src/main/kotlin/Others.kt
@@ -3,6 +3,14 @@ package io.github.com6235.configurator
 import kotlinx.serialization.KSerializer
 import java.io.InputStream
 
+/**
+ * Class for managing file extensions.
+ * Use [addExtension] to add a pair of extension (just a file extension, like "json", "toml", "yaml") and a class,
+ * that implements [Loader].
+ *
+ * @see ConfigLoader
+ * @see Loader
+ */
 class FileExtensions<T : Any>(serializer: KSerializer<T>) {
     @JvmField
     val extensions: MutableMap<List<String>, Loader<T>> = mutableMapOf()
@@ -27,8 +35,11 @@ class FileExtensions<T : Any>(serializer: KSerializer<T>) {
     }
 }
 
-data class Config<T>(val data: T)
-
+/**
+ *  Main class for creating custom loaders.
+ *
+ *  @see FileExtensions
+ */
 abstract class Loader<T : Any>(protected val serializer: KSerializer<T>) {
     abstract fun load(stream: InputStream): T
 }
