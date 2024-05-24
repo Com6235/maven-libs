d184cf25d8355dad881534b22159c5cc70d810d1
Com6235 <ru.nms199@gmail.com>
2024-05-24 16:08:25 +0300

    configurator: make org.jetbrains.kotlinx:kotlinx-serialization-json an api

--- configurator/build.gradle.kts
+++ configurator/build.gradle.kts
@@ -10,7 +10,7 @@ val serializationVersion = "1.6.3"
 dependencies {
     testImplementation(kotlin("test"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:$serializationVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-properties:$serializationVersion")
     implementation("com.charleskorn.kaml:kaml:0.59.0")
